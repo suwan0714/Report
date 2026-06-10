44043d369e90294816d28b37c51c29f6c00aad2c
suwan0714 <crown714@dongyang.ac.kr>
2026-06-10 11:39:38 +0900

    Update kakaoApi.js

--- src/Api/kakaoApi.js
+++ src/Api/kakaoApi.js
@@ -1,55 +1,48 @@
 import axios from "axios";
 
-const REST_API_KEY =
-"331ce42b61116e9a99740098f46050c1";
+const REST_API_KEY = "331ce42b61116e9a99740098f46050c1";
 
-export const searchRestaurant =
-async (keyword) => {
+export const searchRestaurant = async (keyword, currentLocation) => {
+  const response = await axios.get(
+    "https://dapi.kakao.com/v2/local/search/keyword.json",
+    {
+      headers: {
+        Authorization: `KakaoAK ${REST_API_KEY}`,
+      },
 
-  const response =
-    await axios.get(
-      "https://dapi.kakao.com/v2/local/search/keyword.json",
-      {
-        params: {
-          query: keyword,
-        },
-
-        headers: {
-          Authorization:
-            `KakaoAK ${REST_API_KEY}`,
-        },
-      }
-    );
+      params: {
+        query: keyword,
+        x: currentLocation?.lng,
+        y: currentLocation?.lat,
+        radius: 2000,
+        sort: "distance",
+      },
+    },
+  );
 
   return response.data.documents;
 };
 
-export const searchNearbyRestaurant =
-async (x, y) => {
+export const searchNearbyRestaurant = async (x, y) => {
+  const response = await axios.get(
+    "https://dapi.kakao.com/v2/local/search/category.json",
+    {
+      params: {
+        category_group_code: "FD6",
 
-  const response =
-    await axios.get(
-      "https://dapi.kakao.com/v2/local/search/category.json",
-      {
-        params: {
+        x,
+        y,
 
-          category_group_code:
-            "FD6",
+        radius: 2000,
 
-          x,
-          y,
+        sort: "distance",
+      },
 
-          radius: 2000,
-
-          sort: "distance",
-        },
-
-        headers: {
-          Authorization:
-            `KakaoAK ${REST_API_KEY}`,
-        },
-      }
-    );
+      headers: {
+        Authorization: `KakaoAK ${REST_API_KEY}`,
+      },
+    },
+  );
 
   return response.data.documents;
 };
